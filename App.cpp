d91e09cf1ec493fe432bc11432d91349f9d7f214
Onion4070 <naonaosub.0704@gmail.com>
2025-09-13 00:18:06 +0900

    空のGUI作成

---
+++ App.cpp
@@ -0,0 +1,21 @@
+﻿#include "App.h"
+#include "MainFrame.h"
+#include <wx/wx.h>
+
+// インスタンス生成
+wxIMPLEMENT_APP(App);
+
+bool App::OnInit() {
+	// タイトル設定
+	MainFrame* mainFrame = new MainFrame("title: C++ GUI");
+
+	// フレームサイズ設定
+	mainFrame->SetSize(800, 600);
+
+	// フレーム初期位置を中央に
+	mainFrame->Center();
+
+	// フレームを表示
+	mainFrame->Show(true);
+	return true;
+}
---
+++ MainFrame.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+// See https://www.youtube.com/watch?v=cQalRGqRRp4&list=PLFk1_lkqT8MbVOcwEppCPfjGOGhLvcf9G&index=3
+
+#include <wx/wx.h>
+
+class MainFrame : public wxFrame
+{
+public:
+	MainFrame(const wxString& title);
+};
+
